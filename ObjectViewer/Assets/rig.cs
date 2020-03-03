97770cacbba5a0166c2eaa7ca9bd8b2e8cf1c82d
Danalexanderc <31372021+Danalexanderc@users.noreply.github.com>
2020-03-02 22:37:46 -0500

    Project created
    Currently only contains one cube that bounces around

---
+++ ObjectViewer/Assets/rig.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rig : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    // Using FixedUpdate instead of Update because I am using physics
+    void FixedUpdate()
+    {
+        rb.AddForce(0,0,2000 * Time.deltaTime);
+        
+    }
+}
