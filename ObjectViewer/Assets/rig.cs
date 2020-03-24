37d1651684e2a7e003013d9862d3880dcf20e29c
Danalexanderc <31372021+Danalexanderc@users.noreply.github.com>
2020-03-24 13:34:34 -0400

    Rotation and Scale
    Increased rotation speed. Added some free low poly models (from the asset store). Added scale control.

--- ObjectViewer/Assets/rig.cs
+++ ObjectViewer/Assets/rig.cs
@@ -8,19 +8,24 @@ public class rig : MonoBehaviour
 
     public Transform t;
 
-    public float rotX = 0f, rotY = 0f, rotZ = 0f, rotationDelta = 2f;
+    public float rotX = 0f, rotY = 0f, rotZ = 0f, rotationDelta = 7;
 
-    private Vector3 rotation = new Vector3();
+    public Vector3 defaultScale = new Vector3();
+
+    private Vector3 rotation = new Vector3(), scaleChange = new Vector3();
 
     void start()
     {
-
+        
     }
 
     // Using FixedUpdate instead of Update because I am using physics
     void FixedUpdate()
     {
+        rotationDelta = 4;
+        defaultScale.Set(5f, 5f, 5f);
 
+        // Rotation controls
         if (Input.GetKey("u"))
             rotX += rotationDelta;
         if (Input.GetKey("j"))
@@ -34,12 +39,22 @@ public class rig : MonoBehaviour
         if (Input.GetKey("l"))
             rotZ -= rotationDelta;
         if (Input.GetKey("r"))
-        { rotX = 0; rotY = 0; rotZ = 0; }
+        { rotX = 0; rotY = 0; rotZ = 0;
+            t.localScale = defaultScale;
+        }
 
+        // Scale controls
+        if (Input.GetKey("y"))
+            scaleChange.Set(0.1f, 0.1f, 0.1f);
+        else if (Input.GetKey("h"))
+            scaleChange.Set(-0.1f, -0.1f, -0.1f);
+        else
+            scaleChange.Set(0f, 0f, 0f);
+
+        t.localScale += scaleChange;
         rotation.Set(rotX, rotY, rotZ);
         t.localEulerAngles = rotation;
         rb.useGravity = false;
-        //rb.AddForce(0,0,2000 * Time.deltaTime);
 
     }
 }
